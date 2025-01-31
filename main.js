ce4ada065ebc61258810238449117eda2cfd9854
Riko0813 <horiuchi.riko@icloud.com>
2025-01-31 06:20:54 +0000

    セレクターの追加

--- main.js
+++ main.js
@@ -1,53 +1,66 @@
-//function ClickCounterGame(){
-//     let count = 0;
-//     //countという変数を準備して、0を代入する
+const gameSelector = document.getElementById("game-select");
+const gameContainer = document.getElementById("game-container");
+gameSelector.addEventListener('change', function(){
+    gameContainer.innerHTML = '';
+    switch (gameSelector.value){
+        case 'number-guess':
+            startNumberGuessGame();
+            break;
+        case 'clicker':
+             ClickCounterGame();
+            break;
+    }
+});
 
 
-//     const gameContainer = document.getElementById("game-container");
+function ClickCounterGame(){
+     let count = 0;
+     //countという変数を準備して、0を代入する
 
-//     let button1 = document.createElement("button");
-//     let button2 = document.createElement("button");
-//     let button3 = document.createElement("button");
-//     button1.textContent = "+1ボタン";
-//     button2.textContent = "+10ボタン";
-//     button3.textContent = "リセットボタン";
-//     let counter = document.createElement("p");
-//     //ボタンタグを作成する　<buttton>ボタン</button>を作り、変数に代入　１
-//     counter.textContent =  count;
+
+    const gameContainer = document.getElementById("game-container")
+     let button1 = document.createElement("button");
+     let button2 = document.createElement("button");
+     let button3 = document.createElement("button");
+     button1.textContent = "+1ボタン";
+     button2.textContent = "+10ボタン";
+     button3.textContent = "リセットボタン";
+     let counter = document.createElement("p");
+     //ボタンタグを作成する　<buttton>ボタン</button>を作り、変数に代入　１
+    counter.textContent =  count;
     
-//     button1.addEventListener("click",function(){//クリックというイベントを読み取る　２
-//         if (100
-//             >count){
-//         count++;
-//         }
-//         counter.textContent = count; //処理を記述する
-       
-//     })
-//     button2.addEventListener("click",function(){
-//         if (100>count){
-//         count = count + 10;
-//         }
-//         counter.textContent = count;
-//     })
+     button1.addEventListener("click",function(){//クリックというイベントを読み取る　２
+        if (100
+            >count){
+        count++;
+        }
+        counter.textContent = count; //処理を記述する
+            })
+    button2.addEventListener("click",function(){
+        if (100>count){
+        count = count + 10;
+        }         counter.textContent = count;
+        
+     })
 
-//     button3.addEventListener("click",function(){
-//         count = 0;
-//         counter.textContent = count;
+     button3.addEventListener("click",function(){
+         count = 0;
+         counter.textContent = count;
     
-//     })
+     })
     
-//     gameContainer.appendChild(button1);
-//     gameContainer.appendChild(button2);//ボタンを表示する　4
-//     gameContainer.appendChild(button3);
-//     gameContainer.appendChild(counter);
-// }
+     gameContainer.appendChild(button1);
+     gameContainer.appendChild(button2);//ボタンを表示する　4
+    gameContainer.appendChild(button3);
+     gameContainer.appendChild(counter);
+ }
  
-// ClickCounterGame();
+ ClickCounterGame();
 
 
 
 
-function stertNumberGuessGame(){
+function startNumberGuessGame(){
     const gameContainer = document.getElementById("game-container");
     const randomNumber = Math.floor(Math.random()*100) + 1;
     let message = document.createElement("p");
@@ -81,6 +94,8 @@ function stertNumberGuessGame(){
     const countDisplay = document.createElement("p");
     countDisplay.textContent = `試行錯誤 :  ${count}`;
     gameContainer.appendChild(countDisplay);
+
+
 
 }
 stertNumberGuessGame();
